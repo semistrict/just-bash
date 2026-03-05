ac309e579ab4192448f84fd6fe4adf0464229c9a
Malte Ubl <malte.ubl@gmail.com>
2026-03-05 13:00:48 -0800

    Update just-bash in website

--- examples/website/app/components/terminal-content.ts
+++ examples/website/app/components/terminal-content.ts
@@ -736,7 +736,7 @@ limitations under the License.
 
 export const FILE_PACKAGE_JSON = `{
   "name": "just-bash",
-  "version": "2.11.14",
+  "version": "2.11.15",
   "description": "A simulated bash environment with virtual filesystem",
   "repository": {
     "type": "git",
